7c48e686c4a674207da268f9a1f57261423ae8b7
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-02-10 23:35:08 +0530

    Create File.java

---
+++ File.java
@@ -0,0 +1,55 @@
+//Declare and Initialize an Array
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50}; 
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+
+//Largest and Smallest Element in an Array
+
+public class MinMaxArray {
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 5, 67, 89, 1};
+        int min = arr[0], max = arr[0];
+
+        for (int num : arr) {
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
+
+//reverse 
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.print("Reversed Array: ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
+// sum elements 
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
+
