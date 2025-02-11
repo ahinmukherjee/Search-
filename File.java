bdcdb938ef9666ca5755ccc665438fde27ed8028
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-02-11 19:59:26 +0530

    Update File.java

--- File.java
+++ File.java
@@ -53,3 +53,19 @@ public class SumArray {
     }
 }
 
+public class FindElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+        boolean found = false;
+
+        for (int num : arr) {
+            if (num == target) {
+                found = true;
+                break;
+            }
+        }
+        
+        System.out.println("Element " + target + " found: " + found);
+    }
+}
