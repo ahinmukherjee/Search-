f3f4809986544c2736f3066eceb61474245c3761
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-02-25 22:02:25 +0530

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -8,3 +8,14 @@
     <p class="result">Simple Interest: </p>
   </div>
 </div>
+
+<div class="container">
+  <div class="card">
+    <h2>Simple Interest Calculator</h2>
+    <input type="number" placeholder="Enter Amount" />
+    <input type="number" placeholder="Enter Interest (%)" />
+    <input type="number" placeholder="Enter Time (Years)" />
+    <button>Calculate</button>
+    <p class="result">Simple Interest: </p>
+  </div>
+</div>
