68182cdca15339fc6eeff405731cfdc65bff330f
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-03-05 21:30:03 +0530

    Update Change.js

--- Change.js
+++ Change.js
@@ -33,3 +33,58 @@ class Armstrong {
         }
     }
 }
+
+
+
+import java.util.Scanner;
+
+class Practice {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) { // Auto-closing scanner
+            System.out.print("Enter number: ");
+            int n = sc.nextInt(), reverse = 0;
+
+            while (n > 0) {
+                reverse = reverse * 10 + (n % 10);
+                n /= 10;
+            }
+
+            System.out.println("Reverse number is: " + reverse);
+        }
+    }
+}
+
+
+
+import java.util.Scanner;
+
+class Inheritance {
+    private int plus; // Making variable private to follow encapsulation
+
+    void sum(int a, int b) {
+        plus = a + b;
+        System.out.println("Sum = " + plus);
+    }
+
+    int getPlus() {
+        return plus;
+    }
+}
+
+class Y extends Inheritance {
+    void add() {
+        try (Scanner sc = new Scanner(System.in)) { // Auto-closing scanner
+            System.out.print("Enter number: ");
+            int c = sc.nextInt();
+            System.out.println("Result = " + (getPlus() + c));
+        }
+    }
+}
+
+public class X {
+    public static void main(String[] args) {
+        Y y = new Y();
+        y.sum(20, 30);
+        y.add();
+    }
+}
