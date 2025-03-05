dc9bc34dc4c5e8690faab482d8cefc54a645bb9a
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-03-05 21:27:41 +0530

    Update Change.js

--- Change.js
+++ Change.js
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), c = n, arm = 0;
+        sc.close();
+        
+        while (n > 0) {
+            arm += Math.pow(n % 10, 3);
+            n /= 10;
+        }
+        
+        System.out.println(c == arm ? "Number is Armstrong" : "Number is not Armstrong");
+    }
+}
+
+
+import java.util.Scanner;
+
+class Armstrong {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {  // Using try-with-resources to auto-close Scanner
+            int n = sc.nextInt(), temp = n, arm = 0;
+            
+            while (temp > 0) {
+                int r = temp % 10;
+                arm += r * r * r;  // Avoiding Math.pow() to reduce method call overhead
+                temp /= 10;
+            }
+
+            System.out.println(n == arm ? "Number is Armstrong" : "Number is not Armstrong");
+        }
+    }
+}
