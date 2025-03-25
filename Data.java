0d8af141296b46aa9e09d80e82880e52d5f2c12a
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-03-25 23:35:46 +0530

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,251 +1,65 @@
-#include <iostream>
-using namespace std;
+import java.awt.*;  
+import javax.swing.*;  
 
-int main() {
-    int age;
-    cout << "Enter age: ";
-    cin >> age;
+public class FrameTest {  
+    public static void main(String args[]) {  
+        SampleFram st = new SampleFram();  
+        st.setVisible(true);  
+        st.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }  
+}  
 
-    if (age >= 18) {
-        cout << "You are already available for a voter card" << endl;
-    } else {
-        cout << "You are below the required age to apply for a voter card" << endl;
-    }
-
-    return 0;
-}
-
-
-
-#include <stdio.h>
-
-int main() {
-    int age;
-    printf("Enter age: ");
-    scanf("%d", &age);
-
-    if (age >= 18) {
-        printf("You are already available for a voter card\n");
-    } else {
-        printf("You are below the required age to apply for a voter card\n");
-    }
-
-    return 0;
-}
-
-
-import java.util.Scanner;
-
-public class VoterCheck {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter age: ");
-        int age = scanner.nextInt();
+class SampleFram extends JFrame {  
+    public SampleFram() {  
+        Toolkit kit = Toolkit.getDefaultToolkit();  
+        Dimension scrsize = kit.getScreenSize();  
+        int w = scrsize.width;  
+        int h = scrsize.height;  
         
-        if (age >= 18) {
-            System.out.println("You are already available for a voter card");
-        } else {
-            System.out.println("You are below the required age to apply for a voter card");
-        }
-
-        scanner.close();
-    }
-}
-
-
-let age = parseInt(prompt("Enter age:"));
-
-if (age >= 18) {
-    console.log("You are already available for a voter card");
-} else {
-    console.log("You are below the required age to apply for a voter card");
+        setSize(w / 2, h / 2);  
+        setLocation(w / 4, h / 4);  
+        setTitle("Hello");  
+        setResizable(false);  
+    }  
 }
 
 
 
+import java.awt.*;  
+import javax.swing.*;  
 
-#include <stdio.h>
+public class FrameString {  
+    public static void main(String args[]) {  
+        SampleFram st = new SampleFram();  
+        st.setVisible(true);  
+        st.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }  
+}  
 
-int main() {
-    int price;
-    float r = 0, price1 = 0;
-
-    printf("Enter price: ");
-    scanf("%d", &price);
-
-    if (price >= 1500) {
-        r = price * (20.0 / 100);
-        price1 = price - r;
-        printf("Original Price: %d\n", price);
-        printf("Discounted Price: %.2f\n", price1);
-    } else if (price >= 1000) {
-        r = price * (15.0 / 100);
-        price1 = price - r;
-        printf("Original Price: %d\n", price);
-        printf("Discounted Price: %.2f\n", price1);
-    } else if (price >= 500) {
-        r = price * (5.0 / 100);
-        price1 = price - r;
-        printf("Original Price: %d\n", price);
-        printf("Discounted Price: %.2f\n", price1);
-    } else {
-        printf("Not available for discount offer\n");
-    }
-
-    return 0;
-}
-
-
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    int price;
-    float r = 0, price1 = 0;
-
-    cout << "Enter price: ";
-    cin >> price;
-
-    if (price >= 1500) {
-        r = price * (20.0 / 100);
-        price1 = price - r;
-        cout << "Original Price: " << price << endl;
-        cout << "Discounted Price: " << price1 << endl;
-    } else if (price >= 1000) {
-        r = price * (15.0 / 100);
-        price1 = price - r;
-        cout << "Original Price: " << price << endl;
-        cout << "Discounted Price: " << price1 << endl;
-    } else if (price >= 500) {
-        r = price * (5.0 / 100);
-        price1 = price - r;
-        cout << "Original Price: " << price << endl;
-        cout << "Discounted Price: " << price1 << endl;
-    } else {
-        cout << "Not available for discount offer" << endl;
-    }
-
-    return 0;
-}
-
-
-import java.util.Scanner;
-
-public class DiscountCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter price: ");
-        int price = scanner.nextInt();
+class SampleFram extends JFrame {  
+    public SampleFram() {  
+        Toolkit kit = Toolkit.getDefaultToolkit();  
+        Dimension scrsize = kit.getScreenSize();  
+        int w = scrsize.width;  
+        int h = scrsize.height;  
+        setSize(w / 2, h / 2);  
+        setLocation(w / 4, h / 4);  
+        setTitle("Hello");  
+        setResizable(false);  
         
-        double r = 0, price1 = 0;
+        MyPanel p = new MyPanel();  
+        add(p);  
+    }  
+}  
 
-        if (price >= 1500) {
-            r = price * (20.0 / 100);
-            price1 = price - r;
-            System.out.println("Original Price: " + price);
-            System.out.println("Discounted Price: " + price1);
-        } else if (price >= 1000) {
-            r = price * (15.0 / 100);
-            price1 = price - r;
-            System.out.println("Original Price: " + price);
-            System.out.println("Discounted Price: " + price1);
-        } else if (price >= 500) {
-            r = price * (5.0 / 100);
-            price1 = price - r;
-            System.out.println("Original Price: " + price);
-            System.out.println("Discounted Price: " + price1);
-        } else {
-            System.out.println("Not available for discount offer");
-        }
-
-        scanner.close();
-    }
-}
-
-
-let price = parseInt(prompt("Enter price:"));
-let r = 0, price1 = 0;
-
-if (price >= 1500) {
-    r = price * (20 / 100);
-    price1 = price - r;
-    console.log("Original Price:", price);
-    console.log("Discounted Price:", price1);
-} else if (price >= 1000) {
-    r = price * (15 / 100);
-    price1 = price - r;
-    console.log("Original Price:", price);
-    console.log("Discounted Price:", price1);
-} else if (price >= 500) {
-    r = price * (5 / 100);
-    price1 = price - r;
-    console.log("Original Price:", price);
-    console.log("Discounted Price:", price1);
-} else {
-    console.log("Not available for discount offer");
-}
-
-
-#include <stdio.h>
-
-int main() {
-    int num;
-    
-    printf("Enter number: ");
-    scanf("%d", &num);
-
-    if (num % 2 == 0) {
-        printf("Input number is even: %d\n", num);
-    } else {
-        printf("Input number is odd: %d\n", num);
-    }
-
-    return 0;
-}
-
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    int num;
-    
-    cout << "Enter number: ";
-    cin >> num;
-
-    if (num % 2 == 0) {
-        cout << "Input number is even: " << num << endl;
-    } else {
-        cout << "Input number is odd: " << num << endl;
-    }
-
-    return 0;
-}
-
-
-import java.util.Scanner;
-
-public class EvenOdd {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int num = scanner.nextInt();
-        
-        if (num % 2 == 0) {
-            System.out.println("Input number is even: " + num);
-        } else {
-            System.out.println("Input number is odd: " + num);
-        }
-
-        scanner.close();
-    }
-}
-
-let num = parseInt(prompt("Enter number:"));
-
-if (num % 2 === 0) {
-    console.log("Input number is even:", num);
-} else {
-    console.log("Input number is odd:", num);
+class MyPanel extends JPanel {  
+    @Override  
+    public void paintComponent(Graphics g) {  
+        super.paintComponent(g);  
+        setBackground(Color.YELLOW);  
+        Font f = new Font("Serif", Font.BOLD | Font.ITALIC, 15);  
+        g.setFont(f);  
+        g.setColor(Color.RED);  
+        g.drawString("I love You", 100, 200);  
+    }  
 }
