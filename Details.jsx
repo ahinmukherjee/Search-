bbd617bb531cc4ee0f5595c369136658567af442
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-04-06 22:01:44 +0530

    Update Details.jsx

--- Details.jsx
+++ Details.jsx
@@ -1,94 +1,205 @@
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
+class Number:
+    def number_calculate(self, n):
+        if n > 0:
+            print("Number is Positive")
+        elif n < 0:
+            print("Number is Negative")
+        else:
+            print("Number is Equal")
 
-public class TextChange {
-    public static void main(String args[]) {
-        FrameSample st = new FrameSample();
-        st.setVisible(true);
-        st.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+a = int(input("Enter value: "))
+nb = Number()
+nb.number_calculate(a)
+
+
+
+#include <stdio.h>
+
+void numberCalculate(int n) {
+    if(n > 0)
+        printf("Number is Positive\n");
+    else if(n < 0)
+        printf("Number is Negative\n");
+    else
+        printf("Number is Equal\n");
+}
+
+int main() {
+    int a;
+    printf("Enter value: ");
+    scanf("%d", &a);
+    numberCalculate(a);
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+class Number {
+public:
+    void numberCalculate(int n) {
+        if(n > 0)
+            cout << "Number is Positive" << endl;
+        else if(n < 0)
+            cout << "Number is Negative" << endl;
+        else
+            cout << "Number is Equal" << endl;
+    }
+};
+
+int main() {
+    int a;
+    cout << "Enter value: ";
+    cin >> a;
+    Number nb;
+    nb.numberCalculate(a);
+    return 0;
+}
+
+
+
+using System;
+
+class Number {
+    public void numberCalculate(int n) {
+        if (n > 0)
+            Console.WriteLine("Number is Positive");
+        else if (n < 0)
+            Console.WriteLine("Number is Negative");
+        else
+            Console.WriteLine("Number is Equal");
     }
 }
 
-class FrameSample extends JFrame {
-    public FrameSample() {
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension scrsize = kit.getScreenSize();
-        int w = scrsize.width;
-        int h = scrsize.height;
-        setSize(w / 2, h / 2);
-        setLocation(w / 4, h / 4);
-        setTitle("Hello");
-        setResizable(false);
-        MyPanel mp = new MyPanel();
-        add(mp);
+class Program {
+    static void Main() {
+        Console.Write("Enter value: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Number nb = new Number();
+        nb.numberCalculate(a);
+    }
+}
+// program.cs
+
+
+numberCalculate <- function(n) {
+  if (n > 0) {
+    print("Number is Positive")
+  } else if (n < 0) {
+    print("Number is Negative")
+  } else {
+    print("Number is Equal")
+  }
+}
+
+a <- as.integer(readline(prompt = "Enter value: "))
+numberCalculate(a)
+
+
+
+
+class NumberCheck {
+    numberCalculate(n) {
+        if (n > 0) {
+            console.log("Number is Positive");
+        } else if (n < 0) {
+            console.log("Number is Negative");
+        } else {
+            console.log("Number is Equal");
+        }
     }
 }
 
-class MyPanel extends JPanel {
-    JButton yellowButton, greenButton, blueButton, redButton, exitButton;
-    JLabel label;
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    public MyPanel() {
-        yellowButton = new JButton("Yellow");
-        greenButton = new JButton("Green");
-        blueButton = new JButton("Blue");
-        redButton = new JButton("Red");
-        exitButton = new JButton("Exit");
+readline.question("Enter value: ", input => {
+    const a = parseInt(input);
+    const nb = new NumberCheck();
+    nb.numberCalculate(a);
+    readline.close();
+});
 
-        label = new JLabel("Default Text"); // Added JLabel
-        add(label);
 
-        add(yellowButton);
-        add(greenButton);
-        add(blueButton);
-        add(redButton);
-        add(exitButton);
 
-        ColorAction yellowAction = new ColorAction(Color.YELLOW);
-        ColorAction greenAction = new ColorAction(Color.GREEN);
-        ColorAction blueAction = new ColorAction(Color.BLUE);
-        ColorAction redAction = new ColorAction(Color.RED);
 
-        yellowButton.addActionListener(yellowAction);
-        greenButton.addActionListener(greenAction);
-        blueButton.addActionListener(blueAction);
-        redButton.addActionListener(redAction);
 
-        TextAction textYellowButton = new TextAction("Hello");
-        yellowButton.addActionListener(textYellowButton);
+using System;
 
-        ExitAction ea = new ExitAction();
-        exitButton.addActionListener(ea);
-    }
+class Program {
+    static void Main() {
+        Console.Write("Enter value: ");
+        int a = Convert.ToInt32(Console.ReadLine());
 
-    class ColorAction implements ActionListener {
-        Color bgcolor;
-
-        public ColorAction(Color c) {
-            bgcolor = c;
-        }
-
-        public void actionPerformed(ActionEvent ae) {
-            setBackground(bgcolor);
-        }
-    }
-
-    class TextAction implements ActionListener {
-        String bgstring;
-
-        public TextAction(String s) {
-            bgstring = s;
-        }
-
-        public void actionPerformed(ActionEvent ae) {
-            label.setText(bgstring); // Corrected setText()
-        }
-    }
-
-    class ExitAction implements ActionListener {
-        public void actionPerformed(ActionEvent ae) {
-            System.exit(0);
-        }
+        if (a > 0)
+            Console.WriteLine("Number is Positive");
+        else if (a < 0)
+            Console.WriteLine("Number is Negative");
+        else
+            Console.WriteLine("Number is Equal");
     }
 }
+
+
+
+
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Enter value: ", input => {
+    const n = parseInt(input);
+
+    if (n > 0) {
+        console.log("Number is Positive");
+    } else if (n < 0) {
+        console.log("Number is Negative");
+    } else {
+        console.log("Number is Equal");
+    }
+
+    readline.close();
+});
+
+
+
+
+#include <stdio.h>
+
+void main() {
+    int a;
+    printf("Enter value: ");
+    scanf("%d", &a);
+
+    if (a > 0)
+        printf("Number is Positive\n");
+    else if (a < 0)
+        printf("Number is Negative\n");
+    else
+        printf("Number is Equal\n");
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+void main() {
+    int a;
+    cout << "Enter value: ";
+    cin >> a;
+
+    if (a > 0)
+        cout << "Number is Positive" << endl;
+    else if (a < 0)
+        cout << "Number is Negative" << endl;
+    else
+        cout << "Number is Equal" << endl;
+}
+
+
