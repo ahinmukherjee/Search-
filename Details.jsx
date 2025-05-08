106cfe3c42cbb5c3f1e9b5d7c17fa3163893b5f7
Ahin Mukherjee <159748299+ahinmukherjee@users.noreply.github.com>
2025-05-08 23:31:57 +0530

    Update Details.jsx

--- Details.jsx
+++ Details.jsx
@@ -1,42 +1,27 @@
-num <- as.integer(readline(prompt = "Enter number: "))
+import java.util.Scanner;
 
-if (num %% 2 == 0) {
-  cat("Input number is even:", num, "\n")
-} else {
-  cat("Input number is odd:", num, "\n")
-}
+public class StringSwitchExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a color (Red, Blue, Green): ");
+        String color = scanner.nextLine();
 
-
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-readline.question("Enter number: ", function(input) {
-  const num = parseInt(input);
-
-  if (num % 2 === 0) {
-    console.log("Input number is even: " + num);
-  } else {
-    console.log("Input number is odd: " + num);
-  }
-
-  readline.close();
-});
-
-
-
-using System;
-
-class EvenOdd {
-    static void Main() {
-        Console.Write("Enter number: ");
-        int num = int.Parse(Console.ReadLine());
-
-        if (num % 2 == 0) {
-            Console.WriteLine("Input number is even: " + num);
-        } else {
-            Console.WriteLine("Input number is odd: " + num);
+        switch (color) {
+            case "Red":
+                System.out.println("Color is Red");
+                break;
+            case "Blue":
+                System.out.println("Color is Blue");
+                break;
+            case "Green":
+                System.out.println("Color is Green");
+                break;
+            default:
+                System.out.println("Unknown color");
         }
+
+        scanner.close();
     }
 }
+
+
